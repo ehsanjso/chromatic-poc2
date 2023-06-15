6aecd4bc3255146ff9e75ff4c27963d306513f31
Ehsan Jahangirzadeh Soure <ehsan.soure@stackadapt.com>
2023-06-15 17:03:25 -0400

    Update snapshot story

--- src/stories/Button.stories.js
+++ src/stories/Button.stories.js
@@ -10,42 +10,40 @@ export default {
   },
 };
 
+const Template = (args) => <Button {...args} />;
+
 // More on writing stories with args: https://storybook.js.org/docs/react/writing-stories/args
-export const Primary = {
-  args: {
-    primary: true,
-    label: "Button",
-  },
+export const Primary = Template.bind({});
+Primary.args = {
+  primary: true,
+  label: "Button",
 };
 
-export const Secondary = {
-  args: {
-    label: "Button",
-  },
+export const Secondary = Template.bind({});
+Secondary.args = {
+  label: "Button",
 };
 
-export const Large = {
-  args: {
-    size: "large",
-    label: "Button",
-  },
+export const Large = Template.bind({});
+Large.args = {
+  size: "large",
+  label: "Button",
 };
 
-export const Small = {
-  args: {
-    size: "small",
-    label: "Button",
-  },
+export const Small = Template.bind({});
+Small.args = {
+  size: "small",
+  label: "Button",
 };
 
 // src/components/Component.stories.tsx
 
 export const Snapshot = () => (
   <>
-    <Primary />
-    <Secondary />
-    <Large />
-    <Small />
+    <Button {...Primary.args} />
+    <Button {...Secondary.args} />
+    <Button {...Large.args} />
+    <Button {...Small.args} />
   </>
 );
 Snapshot.parameters = {
